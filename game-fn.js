11fe9b38fda95a044991cf9fb401f1ed9aa98730
titinart <k.titinart@gmail.com>
2025-07-17 10:55:31 +0700

    game-fn solutions

--- game-fn.js
+++ game-fn.js
@@ -1,25 +1,34 @@
 "use strict";
-
 const board = [
-	["A", "B", "C"],
-	["D", "E", "F"],
-	["G", "H", "I"],
+    ["A", "B", "C"],
+    ["D", "E", "F"],
+    ["G", "H", "I"],
 ];
-
 let x = 0;
 let y = 0;
+const xMax = board.length - 1;
+const yMax = board[0].length -1;
 const moves = [];
 moves.push(board[x][y]); // Start at A
 
 // Define movement functions
-function moveRight() {}
+function moveRight() { 
+    y < yMax ? moves.push(board[x][++y]) : limitLog('moving right'); 
+}
+function moveLeft() { 
+    y > 0 ? moves.push(board[x][--y]) : limitLog('moving left'); 
+}
+function moveUp() { 
+    x > 0 ? moves.push(board[--x][y]) : limitLog('moving up'); 
+}
 
-function moveLeft() {}
+function moveDown() { 
+    x < xMax ? moves.push(board[++x][y]) : limitLog('moving down'); 
+}
 
-function moveUp() {}
-
-function moveDown() {}
-
+function limitLog(moving) { 
+    console.log( `You have reached the limit of ${moving}.` ); 
+}
 // Call the movement functions
 moveRight(); // Move from A to B
 moveRight(); // Move from B to C
@@ -28,3 +37,43 @@ moveLeft(); // Move from F to E
 
 console.log("Path:", moves.join(" → "));
 console.log("Total Moves (Start from 'A'):", moves.length - 1);
+
+
+// "use strict";
+// const board = [
+//   ["A", "B", "C"],
+//   ["D", "E", "F"],
+//   ["G", "H", "I"],
+// ];
+// let x = 0; // Starting row index
+// let y = 0; // Starting column index
+// const moves = [];
+// moves.push(board[x][y]); // Start at A
+// // Define movement functions
+// // Move right (increase column index)
+// function moveRight() {
+//   y++; 
+//   moves.push(board[x][y]);
+// }
+// // Move left (decrease column index)
+// function moveLeft() {
+//   y--; 
+//   moves.push(board[x][y]);
+// }
+// // Move up (decrease row index)
+// function moveUp() {
+//   x--; 
+//   moves.push(board[x][y]);
+// }
+// // Move down (increase row index)
+// function moveDown() {
+//   x++; 
+//   moves.push(board[x][y]);
+// }
+// // Call the movement functions
+// moveRight(); // Move from A to B
+// moveRight(); // Move from B to C
+// moveDown(); // Move from C to F
+// moveLeft(); // Move from F to E
+// console.log("Path:", moves.join(" → "));
+// console.log("Total Moves (Start from 'A'):", moves.length - 1);
